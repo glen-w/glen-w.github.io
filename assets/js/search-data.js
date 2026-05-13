5b7e74d89ef4fc0180511206e586e9afa87b7542
glen-w <6459622+glen-w@users.noreply.github.com>
2026-05-13 16:20:23 +0000

    Deploying to gh-pages from @ glen-w/glen-w.github.io@ae9664d51525c565606ca9492f5bebe88d780531 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/library/";
           },
-        },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
